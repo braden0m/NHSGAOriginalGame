6894b9969608e55e85065baa182384268dc88971
Yongrong(Grace) Lu <graceluyongrong@gmail.com>
2023-07-31 00:40:19 -0400

    ragdoll ai

--- Save Mi/Assets/RagdollRandomMovement.cs
+++ Save Mi/Assets/RagdollRandomMovement.cs
@@ -30,73 +30,66 @@ public class RagdollRandomMovement : MonoBehaviour //don't forget to change the 
     {
         if (agent.remainingDistance <= agent.stoppingDistance) //done with path
         {
+            Debug.Log("agent");
+            Debug.Log(agent.remainingDistance);
+            Debug.Log(agent.stoppingDistance);
             this.walk = false;
             //this.targetAnimator.SetBool("Walk", this.walk);
-            Vector3 point;
-            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
-            {
-                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f); //gizmos
-                agent.SetDestination(point);
+            Vector3 randomPoint;
+            //if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
+            //{
+            randomPoint = RandomPoint(centrePoint.position, range);
+            agent.SetDestination(randomPoint);
 
-                Vector3 direction = new Vector3(point.x-transform.position.x, 0f, point.z - transform.position.z).normalized;
+                //Vector3 direction = new Vector3(point.x-transform.position.x, 0f, point.z - transform.position.z).normalized;
                 
-                if (direction.magnitude != 0f)
-                {
-                    //float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;// 
-
-                    //hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-                    //hip.transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-                    //hipJoint.targetRotation = Quaternion.Euler(0f, 90f, 0f); 
-                    //Debug.Log(hipJoint.targetRotation);
-                    //this.hip.AddForce(direction * this.speed);
-                    //ragdoll.transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-                    this.walk = true;
-                }
-                else
-                {
+                //if (direction.magnitude != 0f)
+                //{
+                //    this.walk = true;
+                //}
+                //else
+                //{
                     
-                    this.walk = false;
-                }
+                //    this.walk = false;
+                //}
 
                 //this.targetAnimator.SetBool("Walk", this.walk);
-            }
+            //}
 
-            //Vector3 direction = (point-transform.position).normalized;
-            //Debug.Log(direction);
-            //Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, 1f*Time.deltaTime, 0.0f);
-            ////hip.transform.rotation = Quaternion.LookRotation(newDir);
-
-            //// move its hip
-            //float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
-            //this.hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-            //hip.AddForce(direction * this.speed);
-            //Debug.Log(hip.transform.rotation);
-            //this.walk = true;
-
-        
-
-
+     
         }
         //this.targetAnimator.SetBool("Walk", this.walk);
     }
 
-    private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    private Vector3 RandomPoint(Vector3 center, float range)
     {
-
+        Vector3 result;
         Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in sphere 
         NavMeshHit hit;
         if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
         {
+            //NavMeshHit.DrawRay(hit, Vector3.up, Color.blue, 1.0f); //gizmos
             result = hit.position;
-            return true;
+            //return true;
+            return result;
         }
-
+        Debug.Log("nevr");
         result = Vector3.zero;
-        return false;
+        //return false;
+        return result;
     }
 
 
 }
+
+//Vector3 direction = (point-transform.position).normalized;
+//Debug.Log(direction);
+//Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, 1f*Time.deltaTime, 0.0f);
+////hip.transform.rotation = Quaternion.LookRotation(newDir);
+
+//// move its hip
+//float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+//this.hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+//hip.AddForce(direction * this.speed);
+//Debug.Log(hip.transform.rotation);
+//this.walk = true;
