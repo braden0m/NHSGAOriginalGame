8d4c3c78b88c222a5cb24461ce0a9ca79c088e7b
Yongrong(Grace) Lu <graceluyongrong@gmail.com>
2023-07-31 12:11:10 -0400

    Joystick Movement + Fire Extinguisher Fix

--- VR Interactions/Assets/Scripts/Exxample Scripts/JoystickMovement.cs
+++ VR Interactions/Assets/Scripts/Exxample Scripts/JoystickMovement.cs
@@ -5,7 +5,8 @@ using OVR;
 
 public class JoystickMovement : MonoBehaviour
 {
-    public Rigidbody player;
+    public Transform player;
+    public Transform centerCamera;
     public float speed;
 
     void Start()
@@ -21,9 +22,13 @@ public class JoystickMovement : MonoBehaviour
         //var joystick = OVRInput.Get(OVRInput.Get(OVRInput.RawAxis2D.LThumbstick, OVRInput.Controller.LTouch));
         float posY = player.position.y;
 
-        //player.transform.position += transform.forward * joystick.y * speed * Time.deltaTime;
-        //player.transform.position = new Vector3(player.transform.position.x, player.transform.position.x, player.transform.position.x);
+        //player.transform.position += transform.forward * joystickDirection.y * speed * Time.deltaTime;
+        //player.transform.position = new Vector3(player.transform.position.x, player.transform.position.x, player.transform.position.z);
 
-        player.transform.position += Quaternion.Euler(0, player.rotation.eulerAngles.y, 0) * Vector3.forward;
+        //Quaternion.Euler(0, player.rotation.eulerAngles.y, 0)
+
+        player.position += Quaternion.Euler(0, centerCamera.rotation.eulerAngles.y, 0) * new Vector3(joystickDirection.x, 0, joystickDirection.y) * Time.deltaTime;
+
+        //Debug.DrawLine(player.position, player.position + Quaternion.Euler(0, centerCamera.rotation.eulerAngles.y, 0) * new Vector3(joystickDirection.x, 0, joystickDirection.y * 10));
     }
 }
