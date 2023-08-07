d09f6c34ba4898c1efb2be49cbfa06ae22565e53
Yongrong(Grace) Lu <graceluyongrong@gmail.com>
2023-08-07 14:38:24 -0400

    grsce

--- VR Interactions/Assets/Scripts/PlayerDeath.cs
+++ VR Interactions/Assets/Scripts/PlayerDeath.cs
@@ -23,7 +23,7 @@ public class PlayerDeath : MonoBehaviour
     {
         if (volumeProfile.TryGet<Vignette>(out vignetteProfile))
         {
-            //Debug.Log("Got it");
+            Debug.Log("Got it");
         }
         playerAge = 0f;
         stayInFire = false;
@@ -40,29 +40,29 @@ public class PlayerDeath : MonoBehaviour
         //}
         float newIntensity = Mathf.Lerp(0.7f, 1.0f, (float)Math.Sin(lerpTime)/2f+0.5f);
         vignetteProfile.intensity = new ClampedFloatParameter(newIntensity, 0.7f, 1f);
-        
-        
+        Debug.Log(vignetteProfile.intensity);
     }
 
-    private void Update()
-    {
-        if (stayInFire)
-        {
-            // began lerping the intensity
-            ChangeVignette();
-        }
-        else
-        {
-            vignetteProfile.active = false;
-        }
-    }
+    //private void Update()
+    //{
+    //    if (stayInFire)
+    //    {
+    //        vignetteProfile.active = true;
+    //        // began lerping the intensity
+    //        //ChangeVignette();
+    //    }
+    //    else
+    //    {
+    //        vignetteProfile.active = false;
+    //    }
+    //}
     private void OnTriggerStay(Collider other)
     {
         // ! add tag
-        if (!vignetteProfile.IsActive())
-        {
-            vignetteProfile.active = true;
-        }
+        //if (!vignetteProfile.IsActive())
+        //{
+        //    vignetteProfile.active = true;
+        //}
         if (playerAge > 20f)
         {
             Debug.Log("Player DIE");
@@ -72,9 +72,11 @@ public class PlayerDeath : MonoBehaviour
         
         if (other.gameObject.tag == "Fire")
         {
+            vignetteProfile.active = true;
             playerAge += Time.deltaTime;
             vignetteProfile.color = new ColorParameter(Color.Lerp(originalColor, newColor, playerAge / 15f));
             Debug.Log(playerAge);
+            ChangeVignette();
             stayInFire = true;
         }
     }
@@ -83,6 +85,7 @@ public class PlayerDeath : MonoBehaviour
     {
         if (other.gameObject.tag == "Fire")
         {
+            vignetteProfile.active = false;
             //playerAge += Time.deltaTime;
             //Debug.Log(playerAge);
             stayInFire = false;
