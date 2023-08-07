e97fa6b998075d946f916975bbe58a2bca8f1453
DavidGao0518 <dgao2007.primary@gmail.com>
2023-08-07 15:14:58 -0400

    test ragdoll

---
+++ VR Interactions/Assets/NewerRagdollState.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+using UnityEditor;
+using System.Linq;
+public class NewerRagdollState : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private GameObject pelvis;
+    [SerializeField] private GameObject armature;
+
+    private Animator animator;
+    private Collider mainCollider;
+    private Rigidbody mainRigidbody;
+    [SerializeField] private List<Rigidbody> ragdollParts;
+    [SerializeField] private List<Collider> ragdollColliders;
+
+    public List<Transform> waypoints;
+    private int currentWaypointIndex;
+
+    private Vector3 currentVelocity;
+    private bool isGrabbed = false;
+    private Vector3 lastGrabbedLocation;
+    private Coroutine dazedCoroutine;
+    [SerializeField] private bool isMoving = true;
+
+    // Life Manage
+    private float ragAge;
+    public bool canInteract = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mainCollider = armature.GetComponent<Collider>();
+        mainRigidbody = armature.GetComponent<Rigidbody>();
+
+        ragdollColliders = GetComponentsInChildren<Collider>().ToList<Collider>();
+        ragdollParts = GetComponentsInChildren<Rigidbody>().ToList<Rigidbody>();
+
+        ragdollParts.Remove(mainRigidbody);
+        ragdollColliders.Remove(mainCollider);
+        ragdollColliders.Remove(pelvis.GetComponent<Collider>());
+
+        RagdollModeOff();
+        isGrabbed = false;
+        canInteract = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InteractableHeld(SelectEnterEventArgs args)
+    {
+        if (canInteract)
+        {
+            isGrabbed = true;
+
+            RagdollModeOn();
+
+            if (dazedCoroutine != null)
+            {
+                StopCoroutine(dazedCoroutine);
+                dazedCoroutine = null;
+            }
+        }
+
+    }
+
+    public void InteractableUnheld(SelectExitEventArgs args)
+    {
+        if (canInteract)
+        {
+            isGrabbed = false;
+
+            if (dazedCoroutine != null)
+            {
+                StopCoroutine(dazedCoroutine);
+                dazedCoroutine = null;
+            }
+
+            dazedCoroutine = StartCoroutine(Daze());
+        }
+    }
+
+    private void RagdollModeOff()
+    {
+        animator.enabled = true;
+        foreach (Collider ragCol in ragdollColliders)
+        {
+            ragCol.enabled = false;
+        }
+        foreach (Rigidbody rb in ragdollParts)
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void RagdollModeOn()
+    {
+        animator.enabled = false;
+        foreach (Collider ragCol in ragdollColliders)
+        {
+            ragCol.enabled = true;
+        }
+        foreach (Rigidbody rb in ragdollParts)
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+
+    IEnumerator Daze()
+    {
+        //Short stun right after being unheld
+
+        pelvis.transform.position = lastGrabbedLocation;
+
+        yield return new WaitForSeconds(5f);
+        RagdollModeOff();
+        mainRigidbody.velocity = Vector3.zero;
+        lastGrabbedLocation = pelvis.transform.position;
+    }
+}
