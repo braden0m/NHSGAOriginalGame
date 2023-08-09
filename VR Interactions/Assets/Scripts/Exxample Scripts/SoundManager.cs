fd2bce535d5b16530320f15c80950e14434c9e0b
Yongrong(Grace) Lu <graceluyongrong@gmail.com>
2023-08-09 16:33:03 -0400

    stuff

--- VR Interactions/Assets/Scripts/Exxample Scripts/SoundManager.cs
+++ VR Interactions/Assets/Scripts/Exxample Scripts/SoundManager.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private AudioSource au_mixedfire;
+    private AudioSource au_fullsong;
+
+    private void Awake()
+    {
+        foreach (GameObject thing in GameObject.FindGameObjectsWithTag("Breakable"))
+        {
+            if (thing.GetComponent<AudioSource>() != null)
+                thing.GetComponent<AudioSource>().enabled = false;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        au_mixedfire = GetComponentsInChildren<AudioSource>()[0];
+        au_fullsong = GetComponentsInChildren<AudioSource>()[1];
+
+        if (au_mixedfire == null)
+            Debug.LogWarning("can't find");
+        else
+        {
+            Debug.LogWarning("good");
+        }
+            au_mixedfire.Play();
+
+        au_fullsong.Play();
+
     }
 
     // Update is called once per frame
